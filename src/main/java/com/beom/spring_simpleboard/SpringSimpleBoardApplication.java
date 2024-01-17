f9ac991c9db8bad2938ea2fd561a9fc89c1574d5
beom195 <realcriminal195@gmail.com>
2024-01-17 21:46:15 +0900

    project생성

---
+++ src/main/java/com/beom/spring_simpleboard/SpringSimpleBoardApplication.java
@@ -0,0 +1,13 @@
+package com.beom.spring_simpleboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSimpleBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSimpleBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/beom/spring_simpleboard/SpringSimpleBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beom.spring_simpleboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSimpleBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
