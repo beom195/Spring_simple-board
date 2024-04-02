7471f7e2ae89cc0cb655f8b5a20edceb5babf7ad
beom195 <realcriminal195@gmail.com>
2024-04-02 20:32:12 +0900

    에러 페이지 추가, 댓글 수 나오도록 수정, 여러 댓글중 하나 선택해서 update 가능하도록 수정

--- src/main/resources/static/js/comment.js
+++ src/main/resources/static/js/comment.js
@@ -47,7 +47,7 @@ function commentUpdate(event) {
     const updateCancelBtn = commentContainer.querySelector(".comment-update-cancel-btn");
 
 
-    // //버튼을 누른 댓글에 해당 하는 댓글 내용을 가져옴
+    //버튼을 누른 댓글에 해당 하는 댓글 내용을 가져옴
     const comment = commentContainer.querySelector(".comment-content");
     // currentComment = commentContainer.querySelector(".comment-content");
     console.log(comment);
@@ -69,33 +69,12 @@ function commentUpdate(event) {
     commentDeleteBtn.style.display = "inline-block";
     updateCancelBtn.style.display = "inline-block";
 }
-// function commentUpdate() {
-//     //버튼 변수화
-//     const commentUpdateBtn = document.querySelector(".comment-update-btn");
-//     const commentUpdateSaveBtn = document.querySelector(".comment-update-save-btn");
-//     const commentDeleteBtn = document.querySelector(".comment-delete-btn");
-//     const updateCancelBtn = document.querySelector(".comment-update-cancel-btn");
-//     //제목, 내용 변수화
-//     const comment = document.getElementById("update-comment");
-//
-//     //댓글 textArea 타입으로 변경
-//     replaceWithTextArea(comment);
-//
-//     //수정 버튼 클릭시 등록,취소 버튼만 보이게 변경
-//     commentUpdateBtn.style.display = "none";
-//     commentUpdateSaveBtn.style.display = "inline-block";
-//     commentDeleteBtn.style.display = "inline-block";
-//     updateCancelBtn.style.display = "inline-block";
-// }
+
 function commentUpdateSave(){
-    //게시글과 댓글 내용에 담긴 값으로 객체 생성
-    //id에 입력된 값 가져오기
-
-    // const commentContent = currentComment.textContent.trim(); // 현재 댓글 내용을 가져옴
-
     const data = {
         commentId: currentCommentId,
-        comment: document.querySelector(".comment-content").value
+        // comment: document.querySelector(".comment-content").value
+        comment: event.target.closest('.comment').querySelector(".comment-content").value
     }
     console.log("commentId    " + data.commentId);
     console.log("comment   " + data.comment);
@@ -120,7 +99,7 @@ function commentUpdateSave(){
 
 function commentDelete(){
     const data = {
-        commentId: document.querySelector(".commentId").value
+        commentId: currentCommentId
     }
     console.log(data.commentId);
     const cm_delete_check = confirm("댓글을 삭제하시겠습니까?");
@@ -142,87 +121,3 @@ function updateCancel(){
     //취소 버튼 클릭시 페이지 새로고침 (뒤로 가기)
     window.location.reload();
 }
-
-
-// const commentWriteBtn = document.querySelector("#comment-write-btn");
-// commentWriteBtn.addEventListener("click", () =>{
-//
-//     //게시글의 id, 회원 정보 id, 댓글이 담긴 값으로 객체 생성
-//     const data = {
-//         postId: document.querySelector("#postId").value,
-//         userId: document.querySelector("#userId").value,
-//         comment: document.querySelector("#write-comment").value
-//     }
-//     if(!data.comment || data.comment.trim() === ""){
-//         alert("댓글을 입력해주세요!");
-//         return false;
-//     }
-//     console.log("postId" + data.postId);
-//     console.log("userId" + data.userId);
-//     console.log("comment" + data.comment);
-//     $.ajax({
-//         type: "POST",
-//         url: "/commentWrite/" + data.postId,
-//         dataType: "JSON",
-//         contentType: "application/json; charset=utf-8",
-//         data: JSON.stringify(data)
-//     }).done(function () {
-//         window.location.reload();
-//     });
-// });
-
-// //댓글 수정 버튼
-// const commentUpdateBtn = document.querySelector(".comment-update-btn");
-// if(commentUpdateBtn) {
-//     commentUpdateBtn.addEventListener("click", () => {
-//         console.log("댓글 수정 버튼");
-//         //게시글과 댓글 내용에 담긴 값으로 객체 생성
-//         //id에 입력된 값 가져오기
-//         const data = {
-//             commentId: document.querySelector("#commentId").value,
-//             comment: document.querySelector("#update-comment").value
-//         }
-//         if (!data.comment || data.comment.trim() === "") {
-//             alert("댓글을 입력해주세요!");
-//             return false;
-//         }
-//
-//         const cm_update_check = confirm("댓글을 수정하시겠습니까?");
-//         console.log("commentId " + data.commentId);
-//         console.log("comment" + data.comment);
-//         if (cm_update_check === true) {
-//             $.ajax({
-//                 type: "POST",
-//                 url: "/commentUpdate/" + data.commentId,
-//                 dataType: "JSON",
-//                 contentType: "application/json; charset=utf-8",
-//                 data: JSON.stringify(data)
-//             }).done(function () {
-//                 window.location.reload();
-//             });
-//         }
-//     });
-// }
-
-// //댓글 삭제 버튼
-// const commentDeleteBtn = document.getElementBycl("comment-delete-btn");
-// if(commentDeleteBtn) {
-//     commentDeleteBtn.addEventListener("click", () => {
-//         const data = {
-//             commentId: document.getElementById("commentId").value
-//         }
-//         const cm_delete_check = confirm("댓글을 삭제하시겠습니까?");
-//         console.log("commentId", data.commentId);
-//         if (cm_delete_check === true) {
-//             $.ajax({
-//                 type: "DELETE",
-//                 url: "/commentDelete/" + data.commentId,
-//                 dataType: "JSON",
-//                 contentType: "application/json; charset=utf-8",
-//                 data: JSON.stringify(data)
-//             }).done(function () {
-//                 window.location.reload();
-//             })
-//         }
-//     });
-// }
